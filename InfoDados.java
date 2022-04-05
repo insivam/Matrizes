f611caa266b9434ce92b5d43be378a51c2ed29e1
insivam <88207216+insivam@users.noreply.github.com>
2022-04-05 16:20:36 -0300

    Update InfoDados.java

--- InfoDados.java
+++ InfoDados.java
@@ -4,59 +4,6 @@
  * quantas foram as ocorrências da maior pontuação.
  */
 
-/*
-programa
-{
-	inclua biblioteca Util --> u
-	inclua biblioteca Texto --> tx
-	
-	funcao inicio()
-	{
-		cadeia espaco
-		inteiro lista[10], maior = 0, c = 0, valor, i
-		real media = 0.0
-
-		// Bonitin
-		escreva("===================================\n")
-		escreva("        LANÇAMENTO DE DADOS        \n")
-		escreva("===================================\n")
-
-		
-		para(i = 0 ; i < 10 ; i++){ 
-			valor = u.sorteia(1, 6) // faz o sorteio de numeros
-			lista[i] = valor // Coloca o número sorteado na lista
-			media += valor // adiciona o valor sorteado na media
-			se(valor > maior){ 
-				maior = valor // verifica e atualiza a variavel maior
-			}
-
-			// Formata o texto e mostra na tela
-			espaco = tx.preencher_a_esquerda(' ', 9, "")
-			se(i+1 < 10){
-				espaco = tx.preencher_a_esquerda(' ', 10, "")
-			}
-			
-			escreva(espaco, i+1, "º dado: ", valor, "\n")
-			
-		}
-		// Calcula quantidade de vezes o maior número foi sorteado
-		para(i = 0 ; i < 10 ; i++){
-			se(lista[i] == maior){
-				c++
-			}
-		}
-
-		// Mostra os valores previamentes calculados na tela
-		escreva("\n===================================\n")
-		escreva("A média dos números do dado foi ", media/10)
-		escreva("\nO maior número foi: ", maior)
-		escreva("\nO maior número ocorreu ", c, " vezes")
-		escreva("\n===================================\n")
-		
-	}
-
-}
- */
 public class InfoDados {
 
     public static void main(String[] args) {
